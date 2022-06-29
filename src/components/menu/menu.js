29a7d951c4d5a2ed5828c2d5260390c6bbd26f6b
duellal <alexandriaduell@gmail.com>
2022-06-29 16:33:24 -0400

    Menu list is not working properly - working on fixing (Step 6)

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -20,15 +20,62 @@ let menuItems = [
   </div>
 
   The 'menuMaker' takes an array of menu items as its only argument.
+*/
 
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
-  Add those items to the <ul>
+function menuMaker(array) {
+  const menu = document.createElement('div');
+  const list = document.createElement('ul');
 
-  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+  menu.classList.add('menu');
 
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+  menu.appendChild(list);
 
-  Step 5: Don't forget to return your div.menu.
+  /*
+    Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+    Add those items to the <ul>
+  */
 
+  for (let i = 0; i < array.length; i++) {
+    const item = document.createElement('li')
+    const itemText = document.createTextNode(array[i])
+
+    item.appendChild(itemText)
+    list.appendChild(item)
+  }
+
+  /*
+    Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+  */
+
+  const menuBtn = document.querySelector('.menu-button')
+
+  /*
+    Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+  */
+
+  menuBtn.addEventListener('click', (event) => {
+    menu.classList.toggle('menu--open');
+  })
+
+  /*
+    Step 5: Don't forget to return your div.menu.
+  */
+  return menu
+}
+
+/*
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+const header = document.querySelector('.header')
+
+const menuArray = menuItems.map(index => {
+  return menuMaker(index);
+})
+
+menuArray.forEach(element => {
+  header.appendChild(element);
+})
+
+console.log(menuArray)
+console.log(header)
